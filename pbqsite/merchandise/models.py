791ccf1c74a99a3520ae403d151ccff95022ce19
Fang Nan <nanfang05@gmail.com>
2019-04-24 11:56:57 -0700

    add dummy product model

--- pbqsite/merchandise/models.py
+++ pbqsite/merchandise/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(object):
+    def __init__(self, id, name, price, image, count):
+        self.count = count
+        self.id = id
+        self.name = name
+        self.price = price
+        self.image = image
+
+
+products = [
+    Product(1, 'Nice Grill', 100, 'https://i5.walmartimages.ca/images/Large/438/0-1/999999-628915624380-1.jpg', 100),
+    Product(2, 'Powerful Drill', 50, 'https://images-na.ssl-images-amazon.com/images/I/41wMLiA2pyL._SX300_QL70_.jpg',
+            20),
+    Product(3, 'Outdoor Sofa', 250,
+            'https://secure.img2-fg.wfcdn.com/im/25305293/resize-h310-w310%5Ecompr-r85/7216/72169896/belton-patio-sofa-with-cushions.jpg',
+            10)
+]
--- pbqsite/merchandise/views.py
+++ pbqsite/merchandise/views.py
@@ -4,24 +4,24 @@ from io import BytesIO
 from django.shortcuts import render
 import qrcode
 from django.utils.http import urlencode
+from .models import products
 
 
 def index(request):
-    products = []
-    for i in [1, 2]:
-        product = {
-            'id': i,
-            'name': 'Product %s' % i,
-        }
+    codes = {}
+    for product in products:
         scheme = request.is_secure() and "https" or "http"
-        pay_url = '%s://%s/pay/?%s' % (scheme, request.get_host(), urlencode({'to': 'Merchandise Store'}))
+        pay_url = '%s://%s/pay/?%s' % (scheme, request.get_host(),
+                                       urlencode({
+                                           'to': 'Merchandise Store',
+                                           'product_id': product.id,
+                                           'product_name': product.name,
+                                           'pay_for': 'Fang',
+                                       }))
         img = qrcode.make(pay_url)
         buffered = BytesIO()
         img.save(buffered)
         img_str = base64.b64encode(buffered.getvalue()).decode('ascii')
-        product['qrcode'] = img_str
+        codes[product.id] = img_str
 
-        products.append(product)
-
-    return render(request, 'merchandise/index.html', {'products': products})
-
+    return render(request, 'merchandise/index.html', {'products': products, 'codes': codes})
